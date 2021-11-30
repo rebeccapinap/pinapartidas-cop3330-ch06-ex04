a008be30c2aacb2b16fe4a2aff4086c08f3d2614
Rebecca Pina Partidas <rebecca.pinap@gmail.com>
2021-11-29 21:54:18 -0500

    Second Commit

--- pinapartidas-cop3330-ch06-ex04.cpp
+++ pinapartidas-cop3330-ch06-ex04.cpp
@@ -1,11 +1,15 @@
+// Includes header file
 #include "header.h"
 
 int main() 
 {
+    // Creates vector of objects
     vector<Name_value> nameList;
 
+    // Calls function to fill up vector with list of names and scores
     addName(nameList);
 
+    // Calls function to print all objects
     printNames(nameList);
 
     return 0;
@@ -13,6 +17,7 @@ int main()
 
 void addName (vector<Name_value>& nameList)
 {
+    // Data dictionary
     string newName;
     int newValue;
     int i;
@@ -20,14 +25,18 @@ void addName (vector<Name_value>& nameList)
     int nameFlag = 0;
     int whileFlag = 0;
 
+    // While loop that ends if NoName and 0 are entered
     while (whileFlag == 0)
     {
         nameFlag = 0;
         size = nameList.size();
 
+        // While loop that ends if a name is entered that is not duplicate
         while (nameFlag == 0)
         {
             nameFlag = 1;
+
+            // Asks for user input of score
             cout << "Please enter a name: \n"; 
             cin >> newName;
 
@@ -35,6 +44,7 @@ void addName (vector<Name_value>& nameList)
             {
                 if (nameList[i].getName() == newName)
                 {
+                    // Repeats loop if name entered is duplicate
                     cout << "This name has already been entered.\n"; 
                     nameFlag = 0;
                 }
@@ -42,16 +52,19 @@ void addName (vector<Name_value>& nameList)
             }
         }
 
+        // Asks for user input of score
         cout << "Please enter a score: \n"; 
         cin >> newValue;
         cout << "\n"; 
 
+        // Breaks out of while loop and stops asking for names and scores if NoName and 0 are entered
         if (newName == "NoName" && newValue == 0)
         {
             whileFlag = 1;
             break;
         }
 
+        // Adds new object with new name and new scoreand adds it to vector
         Name_value newItem(newName, newValue);
         nameList.push_back(newItem);
     }
@@ -64,6 +77,7 @@ void printNames(vector<Name_value>& nameList)
     int size = nameList.size();
     int i;
 
+    // Prints each name and score
     cout << "List:\n\n"; 
 
     for (i = 0; i < size; i++)
